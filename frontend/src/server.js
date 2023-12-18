55ba4d30f8926505cbb64e9441f0908a96f52cc4
CodingKing2003 <116748107+CodingKing2003@users.noreply.github.com>
2023-12-18 18:23:01 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,7 @@
-export const server = "https://shop0-one.vercel.app/api/v2";
+export const server = "https://eshop-six-beige.vercel.app/api/v2";
 
-export const backend_url="https://shop0-one.vercel.app/";
+export const backend_url="https://eshop-six-beige.vercel.app/";
+
+
 
 
