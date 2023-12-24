b4a568f6efe226ac491a303efc93947641b4c30c
CodingKing2003 <116748107+CodingKing2003@users.noreply.github.com>
2023-12-24 23:12:40 +0530

    Update shop.js

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://multi-vendor-ecommerce-app-ten.vercel.app/${activationToken}`;
+    const activationUrl = `https://multi-vendor-ecommerce-app-ten.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
